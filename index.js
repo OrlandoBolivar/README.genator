86ce5d332551ef3d000ab47214db835a3203642a
OrlandoBolivar <123449213+OrlandoBolivar@users.noreply.github.com>
2023-04-13 23:18:22 +1000

    Added more questions

--- index.js
+++ index.js
@@ -1,66 +1,130 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { dqpb } = require('cli-spinners');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+const { default: Choice } = require('inquirer/lib/objects/choice');
+const licenseOptions = ["MIT", "NU GPLv3", "GNU GPLv3", "GNU LGPLv3"]
 
-const generateREADME = ({Description, InstallationInstructions, UsageInformation, 
-  ContributionGuidelines, TestInstructions }) =>
-  `# <Your-Project-Title>
+const generateREADME = ({ProjectTitle, Motivation, Build, ProblemSolves, InstallationInstructions, UsageInformation, CreditsRepo, CreditsEmail, ContributionGuidelines, License, TestInstructions}) =>
 
-  ## Description
-  ${Description}
- 
-  ## Installation
-  ${InstallationInstructions}
-  ## Usage
-  ${UsageInformation}
-  ## Credits
+  `
+# ${ProjectTitle}
+
+
+## Description
+  - ${Motivation}.
+  - ${Build}
+  - ${ProblemSolves}.
+
+## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#feautures)
+  - [License](#license)  
+  - [Test](#test)
   
-  ## License
+## Installation
+${InstallationInstructions}
+
+## Usage
+${UsageInformation}
+
+
+## Credits
+
+${CreditsRepo}
+${CreditsEmail}
   
-  ## Badges
+## License
+${License}
+
+## Badges
+![LICENSE](https://img.shields.io/badge/${ProjectTitle}-blue)
+![LICENSE](https://img.shields.io/badge/${License}-red)
+
+## Features
   
-  ## Features
+## How to Contribute
+${ContributionGuidelines}
   
-  ## How to Contribute
-  ${ContributionGuidelines}
-  
-  ## Tests
-  ${TestInstructions}
+## Tests
+${TestInstructions}
+
   `
 
 inquirer
   .prompt([
     /* Pass your questions in here */
     {
-        Type: 'input',
-        name: 'Description',
-        message: 'What is the description of your project?',
-        default: 'type your answer'
-    },
+      Type: 'input',
+      name: 'ProjectTitle',
+      message: 'What is the name of your project?',
+      default: 'type your answer'
+  },
     {
-        Type: 'input',
-        name: 'InstallationInstructions',
-        message: 'What are the installation instructions',
-        default: 'type your answer'
-    },
+      Type: 'input',
+      name: 'Motivation',
+      message: 'What was your motivation for this project?',
+      default: 'type your answer'
+  },
     {
-        Type: 'input',
-        name: 'UsageInformation',
-        message: 'What is the usage information?',
-        default: 'type your answer'
-    },
+      Type: 'input',
+      name: 'Build',
+      message: 'Why did you build this project?',
+      default: 'type your answer'
+  },
     {
-        Type: 'input',
-        name: 'ContributionGuidelines',
-        message: 'What were your contribution guidelines?',
-        default: 'type your answer'
-    },
+      Type: 'input',
+      name: 'ProblemSolves',
+      message: 'What problem does this project solve?',
+      default: 'type your answer'
+  },
     {
-        Type: 'input',
-        name: 'TestInstructions',
-        message: 'Insert test instructions',
-        default: 'type your answer'
-    },
+      Type: 'input',
+      name: 'InstallationInstructions',
+      message: 'What are the installation instructions',
+      default: 'type your answer'
+  },
+    {
+      Type: 'input',
+      name: 'UsageInformation',
+      message: 'add the location of the image or clip showing the usage of this project?',
+      default: 'start typing ./'
+  },
+    {
+      Type: 'input',
+      name: 'CreditsRepo',
+      message: 'add your link for your repository?',
+      default: 'paste here'
+  },
+    {
+      Type: 'input',
+      name: 'CreditsEmail',
+      message: 'add your professional email address?',
+      default: 'paste here'
+  },
+    {
+      Type: 'input',
+      name: 'License',
+      message: 'Select your License?',
+      default: 'MIT, NU GPLv, GNU GPLv3, GNU LGPLv3'
+  },
+    {
+      Type: 'input',
+      name: 'ContributionGuidelines',
+      message: 'What were your contribution guidelines?',
+      default: 'type your answer'
+  },
+    {
+      Type: 'input',
+      name: 'TestInstructions',
+      message: 'Insert test instructions',
+      default: 'type your answer'
+  },
+
   ])
   .then((answers) => {
     const readmeMDcontent= generateREADME(answers);
@@ -70,8 +134,4 @@ inquirer
   );
   });
 
-  // .then((data) => {
-  //   const filename = `README.md`
-  //   fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-  //     err ? console.log(err) : console.log('Success!')
-  //   );
+
